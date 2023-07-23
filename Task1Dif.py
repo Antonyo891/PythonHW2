08d5cac58fd227d8140f41369c177778cb77755e
Anton <B_atonyo_S@bk.ru>
2023-07-23 16:27:38 +0300

    Task1Dif completed Decimal

--- Task1Dif.py
+++ Task1Dif.py
@@ -1,15 +1,20 @@
 '''задача 1 сложная необязательная 
 Посчитать сумму цифр любого целого или вещественного числа. Через строку решать нельзя.'''
-import random, os
+import random, os, decimal
 os.system('cls')
-number = float(input("Vvedite 4islo "))#random.randint(1,99999)/10**random.randint(1,4)
+number = random.randint(1,9999999)
+from decimal import Decimal
+afterDecimalPoint = Decimal("0.1")
+afterDecimalPoint1 = Decimal("0.01")
+afterDecimalPoint2 =Decimal("0.001")
+afterDecimalPoint3 = Decimal("0.0001")
+number = number*random.choice([afterDecimalPoint,afterDecimalPoint1,afterDecimalPoint2,afterDecimalPoint3])
 print(number)
 temporaryNumber = number
 while temporaryNumber%1 != 0:
     temporaryNumber*=10
-    print(temporaryNumber)
-sum=0
-while temporaryNumber/10>0:
+    sum=0
+while int(temporaryNumber)/10>0:
     sum=sum+int(temporaryNumber%10)
     temporaryNumber/=10
-print(sum)
+print(f'Summa cifr chisla {number} = {sum}')
