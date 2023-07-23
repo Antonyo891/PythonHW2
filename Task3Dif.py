30e60cc8932b67919ff2bb5b872818f54e1f63d1
Anton <B_atonyo_S@bk.ru>
2023-07-23 22:17:57 +0300

    Completed

---
+++ Task3Dif.py
@@ -0,0 +1,23 @@
+'''задача 3 необязательная
+Перечислить все положительные делители некоторых целых чисел.   арианты посложнее: 23436, 190187200, 380457890232.
+Помогите ей (при помощи функции all_divisors(number), которую напишете сами).Постарайтесь найти самое оптимальное решение.
+Результат представьте в виде списка (не забудьте отсортировать по возрастанию).'''
+import os, random, time
+time1 = time.time()
+os.system('cls')
+def all_divisors (number):
+    divisors = [1,number]
+    divisor = 2
+    i=1
+    temporary = number
+    while divisor < temporary:
+        if number%divisor == 0:
+            divisors.insert(i,divisor)
+            divisors.insert(len(divisors)-(i),number//divisor)
+            temporary = number//divisor
+            i+=1
+        divisor+=1
+    return divisors
+number = random.randint(1,100000)
+print(f'Делители числа {number} - {all_divisors(number)}')
+print(f"Время выполнения кода - {time.time()-time1} секунд")
